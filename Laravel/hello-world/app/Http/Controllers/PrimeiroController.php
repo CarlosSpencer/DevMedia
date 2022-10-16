340c935bb20e8f42174ebfa7f624e4d4179282c2
Carlos Henrique Jochims Spencer de Freitas <45050471+CarlosSpencer@users.noreply.github.com>
2022-10-16 10:23:05 -0300

    estudos com laravel

---
+++ Laravel/hello-world/app/Http/Controllers/PrimeiroController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class PrimeiroController extends Controller
+{
+    public function hello () {
+        return 'Olá, mundo!';
+    }
+}
